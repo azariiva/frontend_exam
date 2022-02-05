e9d9f1ac8ab8f85c8b36eb3dbe647cc391382628
a19680608 <IRusteZaripov@sberbank.ru>
2022-02-05 14:42:58 +0300

    ...

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Card, Spinner} from "react-bootstrap";
+import {Container, Card, Spinner, Row, Col} from "react-bootstrap";
 import {observer} from "mobx-react";
 
 const mapping = {
@@ -13,12 +13,18 @@ const mapping = {
     "earthquake": "Землетрясение"
 }
 
-// function getDescription(event = {}) {
-//     switch (event.type) {
-//         case "acid_rain":
-//             return "${mapping["acid_power"]}"
-//     }
-// }
+function getDescription(event = {}) {
+    switch (event.type) {
+        case "acid_rain":
+            return mapping["acid_power"] + ": " + event.acid_power
+        case "hurricane":
+            return mapping["wind_speed"] + ": " + event.wind_speed
+        case "earthquake":
+            return mapping["earthquake"] + ": " + event.earthquake_power
+        default:
+            return ""
+    }
+}
 
 const EventsPage = observer(() => {
     const [jsonContent, setJsonContent] = React.useState([]);
@@ -35,21 +41,28 @@ const EventsPage = observer(() => {
     return (
         <Container>
             {loading ? (
-                <Spinner animation={"border"} role={"status"} className={"align-jsonContent-center"}>
-                    <span className="visually-hidden">Загрузка...</span>
-                </Spinner>
-            ) : (
-                jsonContent.map(event =>
-                    <Card xs={1} md={2}>
-                        <Card.Title>{mapping[event.type]}</Card.Title>
-                        <Card.Text>
-                            {mapping["victims"]}: {event.victims}
-                        </Card.Text>
-                        <Card.Footer>
-                            {mapping["date"]}: {event.date}
-                        </Card.Footer>
-                    </Card>
-                ))}
+                    <Spinner animation={"border"} role={"status"} className={"align-jsonContent-center"}>
+                        <span className="visually-hidden">Загрузка...</span>
+                    </Spinner>
+                ) :
+                (<Row xs={1} md={2} className={"g-4 mt-1"}>
+                    {jsonContent.map(event =>
+                        <Col>
+                            <Card>
+                                <Card.Title>{mapping[event.type]}</Card.Title>
+                                <Card.Text>
+                                    {getDescription(event)}
+                                </Card.Text>
+                                <Card.Text>
+                                    {mapping["victims"]}: {event.victims}
+                                </Card.Text>
+                                <Card.Footer>
+                                    {mapping["date"]}: {event.date}
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    )}
+                </Row>)}
         </Container>
     )
 })
